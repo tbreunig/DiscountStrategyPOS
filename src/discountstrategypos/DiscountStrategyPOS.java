a20388c6095856d1d318d85637085b7aa888b36b
Tyler <Tyler@10.30.10.155>
2014-02-26 18:16:03 -0600

    Initial repository setup for DiscountStrategyPOS

---
+++ src/discountstrategypos/DiscountStrategyPOS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package discountstrategypos;
+
+/**
+ *
+ * @author Tyler
+ */
+public class DiscountStrategyPOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
